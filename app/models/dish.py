2bb742fe2ecad22133e5374f216c7fb666de5f3b
lisa_ubuntu <belikova_e@garpix.com>
2025-06-13 21:09:49 +0300

    Dish model, header update. Main page image & styles addition

--- app/models/dish.py
+++ app/models/dish.py
@@ -8,18 +8,18 @@ class Dish(CommonNameModelMixin):
     class PRESENTATION(models.TextChoices):
         DELICATESSEN = ('delicatessen', 'Холодные закуски')
         HOT_APPETIZERS = ('hot_appetizers', 'Горячие закуски')
-        FIRST_COURSE = ('first course', 'Первое блюдо: суп')
+        FIRST_COURSE = ('first_course', 'Первое блюдо: суп')
         MAIN_DISH = ('main_dish', 'Горячее')
         DESSERT = ('dessert', 'Десерт')
         BEVERAGES = ('beverages', 'Напитки')
         OTHER = ('other', 'Иное')
 
+    presentation = models.CharField('Подача блюда', choices=PRESENTATION.choices,
+                                    default=PRESENTATION.DELICATESSEN, max_length=35)
     recipe = models.CharField('Рецепт', max_length=5000, blank=True)
     products = models.ManyToManyField('Product', through='DishProduct',  blank=True, related_name='products_dishes')
     cooking_tools = models.ManyToManyField('CookingTool', related_name='tools_dishes', blank=True,
                                            verbose_name='Кухонные инструменты')
-    presentation = models.CharField('Подача блюда', choices=PRESENTATION.choices,
-                                    default=PRESENTATION.DELICATESSEN, max_length=35)
     min_portioning = models.PositiveIntegerField('Минимальная порционность')
 
     class Meta:
